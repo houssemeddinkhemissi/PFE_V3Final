c11be75139d7a46c32730a3a7672f5748912c809
Moetez <moetez.khemissi@esprit.tn>
2022-07-18 12:33:56 +0100

    dada 1

--- app/src/main/java/com/example/pfe_v3final/AccountFragment.java
+++ app/src/main/java/com/example/pfe_v3final/AccountFragment.java
@@ -1,5 +1,6 @@
 package com.example.pfe_v3final;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,7 +16,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class AccountFragment extends Fragment {
-
+    TextView v1;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -58,7 +60,16 @@ public class AccountFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+       View view = inflater.inflate(R.layout.fragment_account, container, false);
+       TextView v1 = (TextView) view.findViewById(R.id.textView8);
+       v1.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent in = new Intent(getActivity(),Editprofileactive.class);
+               startActivity(in);
+           }
+       });
+        return view;
+
     }
 }
