eacbb641b6d7dc2aa31e3eedca21b6479cbed189
Moetez <moetez.khemissi@esprit.tn>
2022-07-18 12:52:00 +0100

    dada 1

---
+++ app/src/main/java/com/example/pfe_v3final/Lostpassword.java
@@ -0,0 +1,14 @@
+package com.example.pfe_v3final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lostpassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lostpassword);
+    }
+}
