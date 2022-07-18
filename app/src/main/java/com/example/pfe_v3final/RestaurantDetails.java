1d9f025b70fcba13526bc4e9c194444848457b5b
Moetez <moetez.khemissi@esprit.tn>
2022-07-18 11:07:13 +0100

    dada 1

--- app/src/main/java/com/example/pfe_v3final/RestaurantDetails.java
+++ app/src/main/java/com/example/pfe_v3final/RestaurantDetails.java
@@ -1,6 +1,7 @@
 package com.example.pfe_v3final;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -13,23 +14,29 @@ import java.util.List;
 
 public class RestaurantDetails extends AppCompatActivity {
         RecyclerView dataList;
-    ArrayList<ProductsModel> dataholder5 ;
+    ArrayList<ProductsModel> dataholder8 ;
+    private RecyclerView.Adapter adapter ;
+    private RecyclerView recyclerViewCategoryList ,recyclerViewShopList ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_details);
-        dataList = findViewById(R.id.recyclerView5);
-        dataholder5 = new ArrayList<>();
+        dataList =findViewById(R.id.recyclerView5);
+        LinearLayoutManager c = new LinearLayoutManager(getBaseContext());
+        c.setOrientation(RecyclerView.HORIZONTAL);
+        dataList.setLayoutManager(c);
+
+        dataholder8 = new ArrayList<>();
         ProductsModel c1 = new ProductsModel(R.drawable.exempleplat,"Poulet Biryani ","Hôtel Ambrosia  \n & Restaurant");
         ProductsModel c2 = new ProductsModel(R.drawable.exempleplat1, "Sauce Tonkatsu","Handi-Restaurant, \n Chittagong");
         ProductsModel c3 = new ProductsModel(R.drawable.exempleplat2,"Poulet Katsu","Hôtel Ambrosia  \n & Restaurant");
-        ProductsModel c4 = new ProductsModel(R.drawable.exempleplat,"12km","12min");
-        ProductsModel c5 = new ProductsModel(R.drawable.exempleplat,"50km","30min");
-        dataholder5.add(c1);
-        dataholder5.add(c2);
-        dataholder5.add(c3);
-        dataholder5.add(c4);
-        dataholder5.add(c5);
+
+        dataholder8.add(c1);
+        dataholder8.add(c2);
+        dataholder8.add(c3);
+        
+      //  dataList.setAdapter(new ProductsModel(dataholder8));
+
 
 
     }
